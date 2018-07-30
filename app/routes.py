358e93d33de43abb012dab73829f24bc8ec59fc2
Nerevarishe <JIyHaTuK@gmail.com>
2018-07-30 12:40:33 +0300

    Adding Sessions

--- app/routes.py
+++ app/routes.py
@@ -13,7 +13,10 @@ def index():
     if form.validate_on_submit():
         search_query = form.searchfield.data
         SL_apteka_ru = 'https://apteka.ru/search/?q=' + search_query
-        response = requests.get(SL_apteka_ru)
+        region_link = 'https://apteka.ru/_action/geoip/setBranch/144/'
+        session = requests.Session()
+        response = session.get(region_link)
+        response = session.get(SL_apteka_ru)
         soup = bs4.BeautifulSoup(response.text)
         drug = soup.find_all("span", "h2-style")
         price = soup.find_all("div", "price")
